e857ab8eead7bf932183da3872fa01a75f35f55c
matt2yu <matt2yu@gmail.com>
2021-07-07 02:13:06 -0500

    Frontend setup cont

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ApplicationRecord.transaction do 
+
+  User.destroy_all
+
+  demo_user = User.create!(first_name: "demo", last_name: "user", email: "demo@email.com", password: "password")
+  demo_user_matt = User.create!(first_name: "Matt", last_name: "Yu", email: "yatt2mu@gmail.com", password: "password")
+  demo_user_josh = User.create!(first_name: "Josh", last_name: "Hernandez", email: "kizzy@gmail.com", password: "password")
+
+
+end
