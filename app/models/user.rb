e553506d9877ad0e01c3c3df6efac0f29ad48482
matt2yu <matt2yu@gmail.com>
2021-07-07 01:38:07 -0500

    Migrated user for user auth start

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,43 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :email, :password_digest, :session_token, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  after_initialize :ensure_session_token
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user
+    return user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    generate_session_token unless self.session_token
+  end
+
+  def generate_session_token
+    self.session_token = SecureRandom.urlsafe_base64
+    while User.find_by(session_token: self.session_token)
+      self.session_token = SecureRandom.urlsafe_base64
+    end
+    self.session_token
+  end
 end
