a542d8cb447b5beb8fca50f04018faf7001c9297
matt2yu <matt2yu@gmail.com>
2021-07-07 01:42:04 -0500

    Backend user auth complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :email, :password_digest, :session_token, presence: true
-  validates :email, uniqueness: true
+  validates :email, :session_token, uniqueness: true
   validates :password, length: { minimum: 6 }, allow_nil: true
 
-  after_initialize :ensure_session_token
+  before_validation :method :ensure_session_token
+  
   attr_reader :password
 
   def self.find_by_credentials(email, password)
@@ -13,31 +14,25 @@ class User < ApplicationRecord
   end
 
   def password=(password)
-    @password = password
     self.password_digest = BCrypt::Password.create(password)
+    @password = password
   end
 
   def is_password?(password)
-    BCrypt::Password.new(self.password_digest).is_password?(password)
+    pw_obj = BCrypt::Password.new(self.password_digest)
+    pw_obj.is_password?(password)
   end
 
   def reset_session_token!
-    self.session_token = generate_session_token
-    self.save!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save
     self.session_token
-  end
+end
 
-  private
+private
 
   def ensure_session_token
-    generate_session_token unless self.session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
   end
 
-  def generate_session_token
-    self.session_token = SecureRandom.urlsafe_base64
-    while User.find_by(session_token: self.session_token)
-      self.session_token = SecureRandom.urlsafe_base64
-    end
-    self.session_token
-  end
 end
