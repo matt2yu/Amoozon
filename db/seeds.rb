4e4165339a83cdd3eaac01dde150679cb8ba3fb2
matt2yu <matt2yu@gmail.com>
2021-07-15 17:47:53 -0500

    Seeding first card

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,25 @@ ApplicationRecord.transaction do
   demo_user_matt = User.create!(first_name: "Matt", last_name: "Yu", email: "yatt2mu@gmail.com", password: "password")
   demo_user_josh = User.create!(first_name: "Josh", last_name: "Hernandez", email: "kizzy@gmail.com", password: "password")
 
-  anime = "Anime"
-  anime1 = Product.create!(name: "Anime show 1", description: "this is a test description", category: anime, price: 69.69, keywords: ["anime", "manga"])
-  anime_review1 = Review.create!(user_id: demo_user.id, product_id: anime1.id, rating: 5, title: "Review 1", body: "MOooooooooooooooooo!!")
-  anime2 = Product.create!(name: "Anime show 2", description: "this is a test description", category: anime, price: 420.00, keywords: ["anime", "manga"])
-  anime_review2 = Review.create!(user_id: demo_user.id, product_id: anime2.id, rating: 4, title: "Review 2", body: "MOooooooooooooooooooooooo!!")
+  electronics = "Electronics"
+  electronics1 = Product.create!(name: "Samsung 50' TV", description: "this is a test description", category: electronics, price: 69.69, keywords: ["anime", "manga"])
+  electronics_review1 = Review.create!(user_id: demo_user.id, product_id: electronics1.id, rating: 5, title: "Review 1", body: "MOooooooooooooooooo!!")
+  electronics2 = Product.create!(name: "Amoozon Fire", description: "this is a test description", category: electronics, price: 420.00, keywords: ["anime", "manga"])
 
-  manga = "Manga"
-  manga1 = Product.create!(name: "Manga book 1", description: "this is a test description", category: manga, price: 123.45, keywords: ["anime", "manga"])
-  manga_review1 = Review.create!(user_id: demo_user.id, product_id: manga1.id, rating: 5, title: "Review 3", body: "MOooooooooooooooooo!!")
-  manga2 = Product.create!(name: "Manga book 2", description: "this is a test description", category: manga, price: 11.11, keywords: ["anime", "manga"])
-  manga_review2 = Review.create!(user_id: demo_user.id, product_id: manga2.id, rating: 4, title: "Review 4", body: "MOooooooooooooooooo!!")
+  # fandg = "Food & Grocery"
+  # manga1 = Product.create!(name: "Manga book 1", description: "this is a test description", category: manga, price: 123.45, keywords: ["anime", "manga"])
+  # manga_review1 = Review.create!(user_id: demo_user.id, product_id: manga1.id, rating: 5, title: "Review 3", body: "MOooooooooooooooooo!!")
+  # manga2 = Product.create!(name: "Manga book 2", description: "this is a test description", category: manga, price: 11.11, keywords: ["anime", "manga"])
+
+  # fandg = "Books and Movies"
+  # manga1 = Product.create!(name: "Manga book 1", description: "this is a test description", category: manga, price: 123.45, keywords: ["anime", "manga"])
+  # manga_review1 = Review.create!(user_id: demo_user.id, product_id: manga1.id, rating: 5, title: "Review 3", body: "MOooooooooooooooooo!!")
+  # manga2 = Product.create!(name: "Manga book 2", description: "this is a test description", category: manga, price: 11.11, keywords: ["anime", "manga"])
+
+  # fandg = "Sports and Outdoors"
+  # manga1 = Product.create!(name: "Manga book 1", description: "this is a test description", category: manga, price: 123.45, keywords: ["anime", "manga"])
+  # manga_review1 = Review.create!(user_id: demo_user.id, product_id: manga1.id, rating: 5, title: "Review 3", body: "MOooooooooooooooooo!!")
+  # manga2 = Product.create!(name: "Manga book 2", description: "this is a test description", category: manga, price: 11.11, keywords: ["anime", "manga"])
 
 
 end
