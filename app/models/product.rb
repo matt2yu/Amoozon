f3c631f8bdc51ea0a8561ab536ada78ab2f378e9
matt2yu <matt2yu@gmail.com>
2021-07-15 16:11:32 -0500

    BE product photo AWS S3

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   validates :name, :description, :category, :price, :keywords, presence: true
-
   has_many :reviews
+  has_one_attached :photo
 
   # Ruby on Rails ActiveStorage method only (average)
   def average_rating
