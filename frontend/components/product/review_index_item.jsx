24ef5b5413d77508e52c232e5308b6dabc4dd529
matt2yu <matt2yu@gmail.com>
2021-07-19 02:05:44 -0500

    presentation ready

--- frontend/components/product/review_index_item.jsx
+++ frontend/components/product/review_index_item.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { CgProfile } from 'react-icons/cg';
 
 const ReviewIndexItem = props => {
   return(
     <li className="review-item">
-      <div>&nbsp;{props.user.first_name}&nbsp;{props.user.last_name}</div>
+      <div><CgProfile />&nbsp;{props.user.firstName}&nbsp;{props.user.lastName}</div>
       <p className="review-title">{props.review.title}</p>
       <p className="review-rating">Rating: {props.review.rating}</p>
       <p className="review-body">{props.review.body}</p>
