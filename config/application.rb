64fc0ef82aaa6bf813e176de044d5cd566fc99e6
matt2yu <matt2yu@gmail.com>
2021-07-20 14:54:40 -0500

    heroku debugging

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-# config/application.rb
-config.assets.initialize_on_precompile = false
 
 module Amoozon
   class Application < Rails::Application
