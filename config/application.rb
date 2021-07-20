5a9f0d9cbf9603179b28ebb6f46bb40c9fedc951
matt2yu <matt2yu@gmail.com>
2021-07-20 14:20:45 -0500

    more change

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+# config/application.rb
+config.assets.initialize_on_precompile = false
 
 module Amoozon
   class Application < Rails::Application
@@ -17,3 +19,5 @@ module Amoozon
     # the framework and any gems in your application.
   end
 end
+
+
