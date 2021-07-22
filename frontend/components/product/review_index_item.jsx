74baa6d0846b8d8a5e7d9acf3edf9a060022ec68
matt2yu <matt2yu@gmail.com>
2021-07-21 22:32:15 -0500

    review index item props

--- frontend/components/product/review_index_item.jsx
+++ frontend/components/product/review_index_item.jsx
@@ -4,7 +4,7 @@ import { GiCow } from 'react-icons/gi';
 const ReviewIndexItem = props => {
   return(
     <li className="review-item">
-      <div><GiCow />&nbsp;{props.user.firstName}&nbsp;{props.user.lastName}</div>
+      <div><GiCow />&nbsp;{props.user.first_name}&nbsp;{props.user.last_name}</div>
       <p className="review-title">{props.review.title}</p>
       <p className="review-rating">Rating: {props.review.rating}</p>
       <p className="review-body">{props.review.body}</p>
